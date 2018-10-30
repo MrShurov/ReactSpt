829d9582e1b9058ce6d419fb38b84be4fa5bb6f0
Egor <shurov13@mail.ru>
2018-10-30 09:13:12 +0300

    feat: login

--- src/components/SptLogin/SptLogin.tsx
+++ src/components/SptLogin/SptLogin.tsx
@@ -3,9 +3,29 @@ import './SptLogin.css';
 
 
 export default class SptLogin extends React.Component {
+
     public render() {
         return (
-            <div>Login</div>
+            <div className="container">
+                <form action="http://localhost:8080/login" method="post">
+                    <h1 className="text-center">Вход в личный кабинет</h1>
+                    <div className="form-group d-flex flex-row justify-content-center">
+                        <div className="col-6">
+                            <input className="form-control" type="text" name="username" id="username"
+                                   placeholder="UserName"/>
+                        </div>
+                    </div>
+                    <div className="form-group d-flex flex-row justify-content-center">
+                        <div className="col-6">
+                            <input className="form-control" type="password" name="password" id="password"
+                                   placeholder="Password"/>
+                        </div>
+                    </div>
+                    <div className="d-flex flex-row justify-content-center">
+                        <input type="submit" className="btn btn-lg btn-primary" value="Вход"/>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
